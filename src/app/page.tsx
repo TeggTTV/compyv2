f0b7eb877e000ab7fc38d5beb56f238b8fc367d6
Joey Jazwinski <doe034945@gmail.com>
2024-08-13 11:29:26 -0400

    asdsad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
 import Landing from "./pages/Landing";
 
-import { PrismaClient } from "@prisma/client";
-
-const db = new PrismaClient();
-
 export default async function Home() {
-  const allUsers = await db.user.findMany({ take: 10, select: { id: true, name: true } });
-  console.log(allUsers);
-
 
   return (
     // router
