1af9461ff9761ae8c006421a0bf974082f66da08
Joey Jazwinski <doe034945@gmail.com>
2024-08-13 11:26:33 -0400

    asd

--- api/webhooks/clerk/route.ts
+++ api/webhooks/clerk/route.ts
@@ -59,5 +59,5 @@ export async function POST(req: Request) {
         console.log("userId:", evt.data.id);
     }
 
-    return new Response("", { status: 200 });
+    return new Response("funny responseeeee", { status: 200 });
 }
